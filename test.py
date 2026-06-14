8d477ea17a25fa15814f6b59c011bc84d14c4d33
Lazar Jevtic <jevragodx@gmail.com>
2026-06-14 14:42:51 +0200

    Remove unnecessary files

--- test.py
+++
@@ -1,9 +0,0 @@
-from imdbinfo import get_movie
-import json
-
-movie = get_movie("27331527", "en")
-
-with open("movie.json", "w", encoding="utf-8") as f:
-    json.dump(movie.model_dump(), f, indent=2, ensure_ascii=False, default=str)
-
-print("Saved to movie.json")
